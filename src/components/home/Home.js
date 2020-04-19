8eb71fa56f981eeb8f5875c6d59c9bec629d6cb8
Ragav25 <ragavravi25@gmail.com>
2020-04-19 18:41:16 +0100

    project-section-completed

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,6 +4,7 @@ import "./HomeStyle.css";
 import logo from "./KRRLogo.svg";
 import TypeWriter from "../typewrite/Typewrite";
 import TypewriteAnimation from "../../typewriteAnimation/TypewriteAnimation";
+import Projects from "../projects/Projects";
 
 let msgs = ["SOFTWARE DEVELOPER", "FREELANCER", "A CODING GEEK!!"];
 
@@ -50,9 +51,51 @@ const Home = () => {
           <TypewriteAnimation />
         </div>
       </section>
-      <section>
-        <h2>hello </h2>
+
+      <section className="project-section">
+        <div className="sidenav-project">
+          <p className="vertical-text">Projects</p>
+        </div>
+
+        <div className="project-heading">
+          <h1 className="heading">Projects</h1>
+        </div>
+
+        <div className="project-01">
+          <Projects />
+        </div>
+        <div className="project-02">
+          <Projects />
+        </div>
+        <div className="project-03">
+          <Projects />
+        </div>
+        <div className="project-04">
+          <Projects />
+        </div>
       </section>
+
+      {/* <section className="project-section">
+        <div className="project-sidenav">
+          <h1 className="vertical-text">PROJECTS</h1>
+        </div>
+
+        <main className="projects-area">
+          <div className="project-1">
+            <Projects />
+          </div>
+          <div className="project-2">
+            <Projects />
+          </div>
+          <div className="project-3">
+            <Projects />
+          </div>
+          <div className="project-4">
+            <Projects />
+          </div>
+        </main>
+      </section> */}
+      <section className="skills-section"></section>
     </div>
   );
 };
