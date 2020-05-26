e90cff8bb8bab5c8372227204ee2b731044dc2e4
Ragav25 <ragavravi25@gmail.com>
2020-05-26 02:13:29 +0100

    added skillsection file

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,14 +2,8 @@ import React, { useState, useEffect } from "react";
 
 import "./HomeStyle.css";
 
-import TypeWriter from "../typewrite/Typewrite";
-
-import ProjectCard from "../projectcard/ProjectCard";
-// import Skills from "../skills/Skills";
-// import IconSkills from "../IconSkills/IconSkills";
 import Contact from "../contact/Contact";
 import About from "../about/About";
-import InfographicSkills from "../inforgraphicSkills/InforgraphicSkills";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -102,51 +96,6 @@ const Home = () => {
 
       <hr></hr>
 
-      {/* <section className="project-section" id="projects">
-        <div className="sidenav-project">
-          <p className="vertical-text">Projects</p>
-        </div>
-
-        <div className="project-heading">
-          <h1 className="heading">Projects</h1>
-        </div>
-
-        <div className="project-01">
-          <ProjectCard
-            projectImg={
-              "https://worldschoolphotographs.com/wp-content/uploads/2018/05/loading.gif"
-            }
-            projectTitle={"Object Search Application with transfer learning"}
-          />
-        </div>
-        <div className="project-02">
-          <ProjectCard
-            projectImg={
-              "https://images.squarespace-cdn.com/content/v1/5c8f97ea77b9034020248bdc/1583250937487-XUA97M0CXER3EC2IO7EX/ke17ZwdGBToddI8pDm48kFQQgP34qnCpeHaeAOzTt7pZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PIvwpK0aFuhG0GtLLHqvbV4raqY38tdDiF-KTEvoUH9G4/Twitter.gif"
-            }
-            projectTitle={"Twitter Clone on google app engine"}
-          />
-        </div>
-        <div className="project-03">
-          <ProjectCard
-            projectImg={
-              "https://cdn.dribbble.com/users/3187414/screenshots/6063216/ezgif.com-crop_2_.gif"
-            }
-            projectTitle={"Anagram Search Engine"}
-          />
-        </div>
-        <div className="project-04">
-          <ProjectCard
-            projectImg={
-              "https://gifimage.net/wp-content/uploads/2017/10/credit-card-gif-3.gif"
-            }
-            projectTitle={"cerdit card fraud detection"}
-          />
-        </div>
-      </section> */}
-
-      {/* new project section */}
-
       <section id="project-container">
         <div className="project-sidebar">
           <p>projects</p>
@@ -187,10 +136,6 @@ const Home = () => {
       <hr></hr>
 
       <section className="skills-section" id="skills">
-        {/* <Skills showTooltip={showTooltip} />
-        <IconSkills id="icon-skills" /> */}
-
-        {/* <InfographicSkills /> */}
         <SkillSection />
       </section>
 
